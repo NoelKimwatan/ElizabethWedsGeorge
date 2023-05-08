63189013fc5438756ecdab3faf9336dd0162b68e
noelkimwatan <noelkimwatan@gmail.com>
2023-05-08 23:57:00 +0300

    Deploying to EBS

--- elizabethandgeorge/settings.py
+++ elizabethandgeorge/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['production-env.eba-bxt2qzzd.eu-west-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "gifts"
+    "gifts",
+    "storages"
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,19 @@ PESAPAL_CONSUMER_SECRET = os.environ.get('PESAPAL_CONSUMER_SECRET')
 PESAPAL_ORDER_REQUEST_URL = os.environ.get('PESAPAL_ORDER_REQUEST_URL')
 PESAPAL_IPN_REGISTRATION_URL = os.environ.get('PESAPAL_IPN_REGISTRATION_URL')
 PESAPAL_GET_TRANSACTION_STATUS_URL = os.environ.get('PESAPAL_GET_TRANSACTION_STATUS_URL')
+
+
+
+#s3 sTATIC STORAGE
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
