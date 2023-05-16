ed8c2449f28748305e3f3f5909c6d7c866b62568
noelkimwatan <noelkimwatan@gmail.com>
2023-05-16 12:29:28 +0300

    .

--- gifts/management/commands/createsuperuser.py
+++ gifts/management/commands/createsuperuser.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-import os
+from elizabethandgeorge.settings import SUPERUSER_USERNAME, SUPERUSER_PASSWORD, SUPERUSER_EMAIL
+
 
 #Used to create a superuser
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username=os.environ['SUPERUSER_USERNAME']).exists():
-            User.objects.create_superuser(os.environ['SUPERUSER_USERNAME'],os.environ['SUPERUSER_EMAIL'],os.environ['SUPERUSER_PASSWORD'])
+        if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+            User.objects.create_superuser(SUPERUSER_USERNAME,SUPERUSER_EMAIL,SUPERUSER_PASSWORD)
